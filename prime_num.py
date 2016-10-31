c6edbb505ebda4fe0cbc7d5d46503f49c3fc1d95
Stephen Oyaro <vystoya@gmail.com>
2016-10-31 22:48:07 +0300

    Range is now well specified.
    Prints all the prime numbers within the given range as expected.

--- prime_num.py
+++ prime_num.py
@@ -1,10 +1,8 @@
-def prime_number_gen(num):
-    lower = 900
-    upper = 1000
-    for num in range(lower,upper + 1): # prime numbers are greater than 1
-        if num > 1:
-            for i in range(2,num):
-                if (num % i) == 0:
-                    break
+def prime_number_gen():
+   for num in range(1,101):
+    for i in range(2,num):
+        if (num%i==0):
+            break
         else:
-        print(num)
+            print(num)
+            break
